a0da52ee13ca028ecbe9ec186164c9b4225c7442
GemsCrown <scanic29@ecolecatholique.ca>
2024-10-15 13:38:42 -0400

    Tiger animation on click event

--- scripts/test.js
+++ scripts/test.js
@@ -3,11 +3,13 @@ const tiger = document.getElementById("tiger");
 const degs = 5;
 let rotation = degs;
 
-setInterval(() => {
-  if (rotation == 365) {
-    rotation = degs;
-  } else {
-    rotation += degs;
-  }
-  tiger.style.transform = `rotate(${rotation}deg)`;
-}, 50);
+tiger.addEventListener("click", () => {
+  setInterval(() => {
+    if (rotation == 365) {
+      rotation = degs;
+    } else {
+      rotation += degs;
+    }
+    tiger.style.transform = `rotate(${rotation}deg)`;
+  }, 50);
+});

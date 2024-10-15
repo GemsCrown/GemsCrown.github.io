e711e2fe7f143991db8d156438b3b6ea62de216c
GemsCrown <scanic29@ecolecatholique.ca>
2024-10-15 11:16:25 -0400

    <main> added

--- scripts/test.js
+++ scripts/test.js
@@ -1,13 +1,13 @@
-const tiger = document.getElementById('tiger')
+const tiger = document.getElementById("tiger");
 
-const degs = 5
-let rotation = degs
+const degs = 5;
+let rotation = degs;
 
 setInterval(() => {
-    if (rotation == 365) {
-        rotation = degs
-    } else {
-        rotation += degs
-    }
-    tiger.style.transform = `rotate(${rotation}deg)`
-}, 50)
+  if (rotation == 365) {
+    rotation = degs;
+  } else {
+    rotation += degs;
+  }
+  tiger.style.transform = `rotate(${rotation}deg)`;
+}, 50);

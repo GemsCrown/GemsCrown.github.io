a50c77b60ad6456982afd5854182f0f136b80780
GemsCrown <scanic29@ecolecatholique.ca>
2024-10-16 09:50:33 -0400

    Idk what, Dad did something with the images

--- scripts/test.js
+++ scripts/test.js
@@ -2,15 +2,31 @@ const tiger = document.getElementById("tiger");
 const images = document.getElementsByTagName("img");
 
 const degs = 5;
-let rotation = degs;
 
-tiger.addEventListener("click", () => {
-  setInterval(() => {
-    if (rotation == 365) {
-      rotation = degs;
+const rotation = [];
+console.log(images);
+Array.from(images).forEach((element, index) => {
+  console.log("element: ", element);
+  console.log("index", index);
+  rotation[index] = {
+    element: element,
+    angle: degs,
+  };
+  rotation[index].angle = degs;
+  rotation[index]?.element.addEventListener("click", () => {
+    if (rotation[index]?.live) {
+      clearInterval(rotation[index].timer);
+      rotation[index].live = false;
     } else {
-      rotation += degs;
+      rotation[index].timer = setInterval(() => {
+        if (rotation[index].angle == 365) {
+          rotation[index].angle = degs;
+        } else {
+          rotation[index].angle += degs;
+        }
+        tiger.style.transform = `rotate(${rotation[index].angle}deg)`;
+        rotation[index].live = true;
+      }, 50);
     }
-    tiger.style.transform = `rotate(${rotation}deg)`;
-  }, 50);
+  });
 });

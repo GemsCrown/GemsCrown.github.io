45d543d02dc19da1d9b76878f1124f283a162c65
GemsCrown <scanic29@ecolecatholique.ca>
2024-10-17 11:39:46 -0400

    Comments js

--- scripts/test.js
+++ scripts/test.js
@@ -1,7 +1,7 @@
 const pageImages = document.getElementsByTagName("img");
 const degs = 5;
 const images = [];
-
+//le degs dit au image combien tourner
 Array.from(pageImages).forEach((element, index) => {
   console.log("element: ", element);
   console.log("index", index);
@@ -9,7 +9,7 @@ Array.from(pageImages).forEach((element, index) => {
     element: element,
     angle: degs,
     live: false,
-  };
+  }; //Ceci keeps track de combien l'image a déjà tourné
   images[index]?.element.addEventListener("click", () => {
     if (images[index].live) {
       clearInterval(images[index].timer);
@@ -26,6 +26,6 @@ Array.from(pageImages).forEach((element, index) => {
         ].element.style.transform = `rotate(${images[index].angle}deg)`;
         images[index].live = true;
       }, 50);
-    }
+    } //Et ca dit au programe qu'il faut activer quand il y a un clique. Si il tourne déjà quand il y a un clique il faut arréter ou il était, etc..
   });
 });
